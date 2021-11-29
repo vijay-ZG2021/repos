529f3b2438378f7d74c1239d191272d27ed377be
Vijay Lakshmi <vijay.lakshmi@zaisgroup.com>
2021-11-29 16:56:00 -0500

    files modified

--- EverestEquity/Source/LoadFile.py
+++ EverestEquity/Source/LoadFile.py
@@ -1,11 +1,13 @@
 import pyodbc
 import csv
 import chardet
-import pandas
 from shutil import copyfile
 from datetime import datetime
-from typing import Any
-
+#from typing import Any
+import configparser
+from pathlib import Path
+import os
+import sys
 
 def upload_table(path,filename,delim,cursor,encoding):
     try:
@@ -15,9 +17,8 @@ def upload_table(path,filename,delim,cursor,encoding):
         with open(path+filename, 'r',encoding=encoding) as f:
             reader = csv.reader(f,delimiter =delim)
             for row in reader:
-                row.pop()
                 row = ['NULL' if val=='' else val for val in row]
-                #row = [x.replace("'","''") for x in row]
+                row = [x.replace("'","''") for x in row]
                 if cnt==0:
                     cols =  ",".join(item for item in row) 
                     cols = cols.replace("'","")
@@ -32,52 +33,84 @@ def upload_table(path,filename,delim,cursor,encoding):
                 if cnt%10 == 0:
                     cursor.commit()
             cursor.commit()
-            print("uploaded " + str(cnt) + " rows into table "+ tbl + ".")
-
+       
     except Exception as e:
-            print("--------------------------------------------")
+            print("--------------------Error------------------------")
             print(e)
 
-def run_proc(procname,cursor,parameters):
-    cursor.callproc(procname,parameters)
+def run_proc(cursor):
+    try:
+        values = (datetime.now().strftime('%Y-%m-%d'))
+        sql = " EXEC [everest].[pr_processEquityAllocations] @report_file_date='"+values + "'"
+        cursor.execute(sql)
+        cursor.commit()
+    except Exception as e:
+            print("-------------------Error-------------------------")
+            print(e)
 
-def process_file(sourcepath,destpath,filename):
+def process_file(sourcepath,filename):
     now =datetime.now()
     date_time=now.strftime("%m%d%Y")
-    copyfile(sourcepath+filename,destpath+filename+date_time+'.processed')
+    copyfile(sourcepath+filename,sourcepath+"TradeCapture/"+filename+date_time+'.processed')
  
-#f = open ("//app-everestprod/EVERESTdownload/zais/Extracts/EquityTradeReport.csv","rb")
-f = open ("C:/TradeExport/Extracts/TradesbyCounterparty-EquityOnly.csv","rb")
-data = f.read()
-f.close()
-encoding = chardet.detect(data)['encoding']
+def get_connection():
+    try:
+        config = configparser.ConfigParser()
 
-server = 'SQL-Staging'
-database= 'ZaisOperation'
-username = "appuser"
-password = 'zais4ppd3v'
-# conn_str = (
-#     r'DRIVER={SQL Server};'
-#     r'SERVER=(local)\SQLEXPRESS;'
-#     r'DATABASE=myDb;'
-#     r'Trusted_Connection=yes;'
-# )
-cnxn = pyodbc.connect('DRIVER={SQL Server Native Client 11.0};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
-cursor = cnxn.cursor()
+        if hasattr(sys, 'frozen'):
+        # retrieve path from sys.executable
+            path_current_directory = os.path.abspath(os.path.dirname(sys.executable))
+        else:
+        # assign a value from __file__
+            path_current_directory = os.path.abspath(os.path.dirname(__file__))
 
-#upload_table("//app-everestprod/EVERESTdownload/zais/Extracts/","EquityTradeReport.csv",",",cursor,encoding)
-#process_file("//app-everestprod/EVERESTdownload/zais/Extracts/","//app-everestprod/EVERESTdownload/zais/Extracts/TradeCapture/","EquityTradeReport.csv")
+        #path_current_directory = os.path.dirname(__file__)
+        path_config_file = os.path.join(path_current_directory,  'LoadFile.config')
+        config.read(path_config_file, encoding = 'utf-8-sig')
+        #config.read(Path.dirname(Path.abspath("__file__")).parent.joinpath("LoadFile.config"),encoding = 'utf-8-sig')
+        server = config.get("CoreContext","server")
+        db=  config.get('CoreContext','database')
+        user =  config.get('CoreContext','user')
+        password = config.get('CoreContext','password')
+        cnxn = pyodbc.connect('DRIVER={SQL Server Native Client 11.0};SERVER='+server+';DATABASE='+db+';UID='+user+';PWD='+ password)
+        return cnxn
 
-upload_table("C:/TradeExport/Extracts/","TradesbyCounterparty-EquityOnly.csv",",",cursor,encoding)
-run_proc("[everest].[pr_processEquityAllocations]", cursor, datetime.now().strftime("%m%d%Y"))
-process_file("C:/TradeExport/Extracts/","C:/TradeExport/Extracts/TradeCapture/","TradesbyCounterparty-EquityOnly.csv")
+    except Exception as e:
+            print("--------------------Error------------------------")
+            print(e)
+ 
+try:
+    config = configparser.ConfigParser()
 
-#upload_table("C:/TradeExport/Extracts/","DAILYZAISTCIMPORT.csv",",",cursor,encoding)
-#run_proc("[everest].[pr_processAllocations]", cursor, datetime.now().strftime("%m%d%Y"))
-#process_file("C:/TradeExport/Extracts/","C:/TradeExport/Extracts/TradeCapture/","EquityTradeReport.csv")
+    if hasattr(sys, 'frozen'):
+          # retrieve path from sys.executable
+        path_current_directory = os.path.abspath(os.path.dirname(sys.executable))
+    else:
+        # assign a value from __file__
+        path_current_directory = os.path.abspath(os.path.dirname(__file__))
 
-cursor.close()
-cnxn.close()
+    #path_current_directory = os.path.dirname(__file__)
+    path_config_file = os.path.join(path_current_directory,  'LoadFile.config')
+    config.read(path_config_file, encoding = 'utf-8-sig')
+ 
+    sourcepath = config.get('CoreContext','sourcepath')
+    filename = config.get('CoreContext','filename')
+    f = open(sourcepath + filename,"rb")
+    data = f.read()
+    f.close()
+    encoding = chardet.detect(data)['encoding']
+    cnxn = get_connection()
+    cursor = cnxn.cursor()
 
+    upload_table(sourcepath,filename,",",cursor,encoding)
+    run_proc(cursor)
+    process_file(sourcepath,filename)
+
+    cursor.close()
+    cnxn.close()
+
+except Exception as e:
+        print("-------------------Error-------------------------")
+        print(e)
 
 
